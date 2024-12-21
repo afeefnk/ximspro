17e11cd4b2c88a748712c507b85ee6e465a838ab
Mohammed Afeef NK <connnectafeefnk@gmail.com>
2024-12-21 12:30:15 +0530

    ximspro

--- src/App.jsx
+++ src/App.jsx
@@ -5,35 +5,31 @@ import "./index.css";
 import AdminLogin from "./pages/AdminLogin/AdminLogin";
 import Password from "./pages/ForgotPassword/Password";
 
-// import Layout from "./pages/Layout";
-// import Dashboard from "./pages/Dashboard/Dashboard";
-// import Companies from "./pages/Companies/Companies";    
-// import AddCompany from "./pages/AddCompany/AddCompany"; 
-// import ViewCompany from "./pages/ViewCompany/ViewCompany";
-// import EditCompany from "./pages/EditCompany/EditCompany";
+import Layout from "./pages/Layout";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Companies from "./pages/Companies/Companies";    
+import AddCompany from "./pages/AddCompany/AddCompany" 
+import ViewCompany from "./pages/ViewCompany/ViewCompany";
+import EditCompany from "./pages/EditCompany/EditCompany";
 
 const App = () => {
   return (
-    <div className="App">
-      {/* Use BrowserRouter here */}
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<AdminLogin />} />
-          <Route path="/forgotpassword" element={<Password />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<AdminLogin />} />
+          <Route path="forgotpassword" element={<Password />} />
+        </Route>
 
-          {/* Uncomment these routes if needed */}
-          {/* 
-          <Route path="/admin" element={<Layout />}>
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="companies" element={<Companies />} />
-            <Route path="addcompany" element={<AddCompany />} />
-            <Route path="viewcompany/:companyId" element={<ViewCompany />} /> 
-            <Route path="editcompany/:companyId" element={<EditCompany />} /> 
-          </Route> 
-          */}
-        </Routes>
-      </BrowserRouter>
-    </div>
+        <Route path="/admin" element={<Layout/>}>
+          <Route path="dashboard" element={<Dashboard/>}/>
+          <Route path="companies" element={<Companies/>}/>
+          <Route path="addcompany" element={<AddCompany/>}/>
+          <Route path="viewcompany/:companyId" element={<ViewCompany />} /> 
+          <Route path="editcompany/:companyId" element={<EditCompany />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
